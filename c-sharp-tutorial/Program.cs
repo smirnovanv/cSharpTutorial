91a3fbf11f9b435df85d2ce4ccde5398c2fe6291
natalya-smirnova <v-nsmirnova@microsoft.com>
2021-12-27 14:29:50 +0300

    Enums

---
+++ c-sharp-tutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c_sharp_tutorial
+{   
+    public enum ShippingMethod
+    {
+        Regular = 1,
+        Registered = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
